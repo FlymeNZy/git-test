1df42630743d5ffd5b186d23ce189a206fe4151e
FlymeNZy <FlymeNZy@qq.com>
2022-04-27 22:27:43 +0800

    3

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,13 +1,14 @@
 package com.atguigu.git;
 
 /**
- * @author FlymeNZy
- * @create 2022-04-27 21:11
+ * author FlymeNZy
+ * create 2022-04-27 21:11
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git1!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
