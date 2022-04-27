671ee2efaacb8bfcf29a09bf5291981cbee26e2b
FlymeNZy <FlymeNZy@qq.com>
2022-04-27 21:13:25 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author FlymeNZy
+ * @create 2022-04-27 21:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
